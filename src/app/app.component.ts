80a514bc36141a706297988bfb5a3ae09b2e55c9
Shawn-imran <md.almahmudimran@gmail.com>
2022-01-11 12:26:27 +0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  list:any[]=[];
+
+  addTask(item:string){
+    if (item!=="") {
+      this.list.push({id:this.list.length, name:item});
+    console.warn(this.list);
+    } else {
+      alert("Please enter your task");
+    }
+  }
+  deleteTask(id:number){
+    console.warn(id);
+    this.list=this.list.filter(item => item.id!==id);
+  }
+  clearTasks(){
+    this.list.length=0;
+  }
+
 }
